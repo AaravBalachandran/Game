b3222d2a64690a3ec6c1dc5e701b832904342136
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2022-11-22 01:50:45 +0530

    Basic touch working

---
+++ core/src/com/firstgame/game/VsFriend.java
@@ -0,0 +1,47 @@
+package com.firstgame.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import static com.badlogic.gdx.Gdx.graphics;
+
+public class VsFriend implements Screen {
+
+    final main mainLoad;
+    SpriteBatch batch;
+    Texture start;
+    Sprite sprite_start;
+
+    public VsFriend(main mainLoad) {
+        this.mainLoad = mainLoad;
+        batch = new SpriteBatch();
+        start = new Texture("GamePlay/Images/background/theme2.png");
+        sprite_start = new Sprite(start);
+        sprite_start.setSize(graphics.getWidth(), graphics.getHeight());
+    }
+
+    public void render(float delta) {
+        ScreenUtils.clear(0.95F, 0.95F, 0.95F, 0.95F);
+        batch.begin();
+        sprite_start.draw(batch);
+        batch.end();
+    }
+
+    public void dispose() {
+        batch.dispose();
+    }
+
+    @Override
+    public void show() {}
+    @Override
+    public void resize(int width, int height) {}
+    @Override
+    public void pause() {}
+    @Override
+    public void resume() {}
+    @Override
+    public void hide() {}
+}
