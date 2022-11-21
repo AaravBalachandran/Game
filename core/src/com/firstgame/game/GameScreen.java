feb7ea7573fc5c0f2140682dd9134ed42517349b
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2022-11-21 22:25:09 +0530

    inter2

--- core/src/com/firstgame/game/GameScreen.java
+++ core/src/com/firstgame/game/GameScreen.java
@@ -18,15 +18,15 @@ public class GameScreen implements Screen {
     Texture start;
     Texture tankStar;
     Sprite sprite_start;
-    Sprite sprite_tankStars;
+    Sprite sprite_tank1;
     Music menuMusic;
     public GameScreen(main mainLoad) {
         this.mainLoad = mainLoad;
         batch = new SpriteBatch();
-        start = new Texture("Menu/Images/theme1.png");
-        tankStar = new Texture("Menu/Images/blueTank.png");
+        start = new Texture("Menu/Images/main.png");
+        tankStar = new Texture("Menu/Images/Tanks/tank2.png");
         sprite_start = new Sprite(start);
-        sprite_tankStars = new Sprite(tankStar);
+        sprite_tank1 = new Sprite(tankStar);
         sprite_start.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         menuMusic = Gdx.audio.newMusic(Gdx.files.internal("Menu/Music/mainMenu.mp3"));
         menuMusic.setLooping(true);
@@ -35,14 +35,16 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.95F, 0.95F, 0.95F, 0.95F);
+//        ScreenUtils.clear(0.95F, 0.95F, 0.95F, 0.95F);
         batch.begin();
         sprite_start.draw(batch);
-        sprite_tankStars.draw(batch);
-        sprite_tankStars.setPosition(graphics.getWidth()/2-sprite_tankStars.getWidth()/2, graphics.getHeight()/2-sprite_tankStars.getHeight()/2);
+        sprite_tank1.draw(batch);
+        sprite_tank1.setSize(500,318);
+//        1.71372549
+        sprite_tank1.setPosition(graphics.getWidth()/4 -sprite_tank1.getWidth()/4, graphics.getHeight()/4-sprite_tank1.getHeight()/4);
         batch.end();
         if (Gdx.input.isTouched()) {
-            Rectangle bounds = new Rectangle(graphics.getWidth()/2-sprite_tankStars.getWidth()/2, graphics.getHeight()/2-sprite_tankStars.getHeight()/2,501,284); //corners of the image/texture
+            Rectangle bounds = new Rectangle(graphics.getWidth()/2-sprite_tank1.getWidth()/2, graphics.getHeight()/2-sprite_tank1.getHeight()/2,501,284); //corners of the image/texture
             Vector3 touchPoint = new Vector3();
             touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             if (bounds.contains(touchPoint.x, touchPoint.y)) {
