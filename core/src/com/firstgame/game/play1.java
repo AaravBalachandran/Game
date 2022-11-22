c119d0ef947b130124cfffd418318b8aa3ab82f5
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2022-11-22 21:05:05 +0530

    buttons

--- core/src/com/firstgame/game/play1.java
+++ core/src/com/firstgame/game/play1.java
@@ -16,14 +16,41 @@ public class play1 implements Screen {
     private Stage gameStage;
     Image load;
     Image terrain;
+    Image healthP1;
+    Image healthP2;
+    Image vs;
+    Image tank1;
+    Image tank2;
     Music gameSound;
+
     public play1(final TankStars game) {
         this.game = game;
         gameStage = new Stage();
-        load = new Image(new Texture(Gdx.files.internal("GamePlay/Images/background/theme/theme1.png")));
+        load = new Image(new Texture(Gdx.files.internal("GamePlay/Images/background/theme/theme2.png")));
         load.setSize(graphics.getWidth(), graphics.getHeight());
         terrain = new Image(new Texture(Gdx.files.internal("GamePlay/Images/background/terrain/STerrain.png")));
         terrain.setSize(graphics.getWidth(), graphics.getHeight()-360);
+
+        healthP1 = new Image(new Texture(Gdx.files.internal("GamePlay/Images/background/Components/health2.png")));
+        healthP1.setSize(402,90);
+        healthP1.setPosition(414,850);
+
+        healthP2 = new Image(new Texture(Gdx.files.internal("GamePlay/Images/background/Components/health1.png")));
+        healthP2.setSize(402,90);
+        healthP2.setPosition(900,850);
+
+        vs = new Image(new Texture(Gdx.files.internal("GamePlay/Images/background/Components/vs2.png")));
+        vs.setSize(200,200);
+        vs.setPosition(760,790);
+
+        tank1 = new Image(new Texture(Gdx.files.internal("Menu/Images/spaceTanks/blueTank.png")));
+        tank1.setSize(115,55);
+        tank1.setPosition(1404,341);
+
+        tank2 = new Image(new Texture(Gdx.files.internal("Menu/Images/spaceTanks/Tank4.png")));
+        tank2.setSize(115,75);
+        tank2.setPosition(375,408);
+
         Gdx.input.setInputProcessor(gameStage);
         gameSound = Gdx.audio.newMusic(Gdx.files.internal("GamePlay/Music/gamePlay.mp3"));
         gameSound.setLooping(true);
@@ -31,6 +58,11 @@ public class play1 implements Screen {
 
         gameStage.addActor(load);
         gameStage.addActor(terrain);
+        gameStage.addActor(healthP1);
+        gameStage.addActor(healthP2);
+        gameStage.addActor(vs);
+        gameStage.addActor(tank1);
+        gameStage.addActor(tank2);
     }
 
     @Override
