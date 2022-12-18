519f3b0348c6d719eee772123069a3617173c872
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2022-12-18 15:14:18 +0530

    Following a video

--- core/src/com/firstgame/game/TankStars.java
+++ core/src/com/firstgame/game/TankStars.java
@@ -2,7 +2,6 @@ package com.firstgame.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.firstgame.game.screen.LoadScreen;
 
 public class TankStars extends Game {
